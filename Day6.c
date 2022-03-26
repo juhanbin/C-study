0b8c91490762e7093e7d3eac61e1eed986091a57
juhanbin <gksqls0509@naver.com>
2022-03-26 19:52:21 +0900

    제어문자와 형변환

---
+++ Day6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+//애완 동물 정보
+char * name="고양이";
+int age=3;
+char gender='M';
+float weight=6.3;
+
+//제어문자
+//반드시 따옴표 안에서 작성한다
+// \ㅜ: new line(줄바꿈, 개행문자)
+// \t: tab(위 아래 줄 간격 맞추어 띄기)
+// \\: \표현
+// \': '표현
+// \": "표현
+//printf("이름:%s\n",name);
+//puts(): 문자열 출력, 마지막에는 줄바꿈이 자동으로 삽입된다.
+puts(name);
+printf("나이:%d\n",age);
+printf("성별:%c\n",gender);
+printf("몸무게:%.1fkg\n",weight);
+}
