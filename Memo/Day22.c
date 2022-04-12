bb866ce3c2a69b6531eb27a5e84942895d7d9655
juhanbin <gksqls0509@naver.com>
2022-04-12 16:12:45 +0900

    포인터 기초와 배열

---
+++ Memo/Day22.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+    int data=10;
+    int * pData=&data;
+    printf("%p\n",&data);
+    printf("%p\n",pData);
+    printf("%d\n",*pData);
+    printf("%p\n",pData+1);
+    printf("%p",&data+1);
+}
