7695bb2dd9d0282c0f753b0d4e1a7d787378e2ca
juhanbin <gksqls0509@naver.com>
2022-04-14 23:58:07 +0900

    배열 예제2

---
+++ Day24.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+{
+    // //1~100까지 배열에 담은 후 홀수만 출력
+    // int arData[100]={0,};
+
+    // for(int i=0;i<100;i++)
+    // {
+    //     arData[i]=i+1;
+    //     if(arData[i] % 2 == 1)
+    //     {
+    //         printf("%d\n",arData[i]);
+    //     }
+    // }
+
+    //1~100까지 배열에 담은 후 짝수의 합 출력
+    // int arData[100]={0,};
+    // int total=0;
+    // for(int i=0;i<100;i++)
+    // {
+    //     arData[i]=i+1;
+    //     if(arData[i]%2==0)
+    //     {
+    //         total+=arData[i];
+    //     }
+    // }
+    // printf("%d\n",total);
+
+
+}
