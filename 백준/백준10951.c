3925856f547999b1ceb6da96cdeeeb63031ffc05
juhanbin <gksqls0509@naver.com>
2022-05-10 20:22:02 +0900

    13번

--- 백준/백준10951.c
+++ 백준/백준10951.c
@@ -1,12 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    int A,B=0;
-    while (1)
-    {
-        scanf("%d %d",&A,&B);
-        printf("%d\n",A+B);
-    }
-    return 0;
+int main() {
+	int A, B;
+	while (scanf("%d %d", &A, &B) != EOF)
+		printf("%d\n", A + B);
+	return 0;
 }
